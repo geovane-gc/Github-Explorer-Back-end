17b425acce9edc4a723f5cc9f0ec99b9f0e54111
Geovane <geovanegallotticazal@hotmail.com>
2020-10-14 09:36:52 -0300

    creating routes

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+
+    return response.json({ appointment });
+});
+
+export default appointmentsRouter;
